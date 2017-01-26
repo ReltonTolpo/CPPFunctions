fa117303f3833ba32569e21f5925ede5f362fff8
ReltonTolpo <ori.taylor@gmail.com>
2017-01-26 16:45:02 +0000

    Adding all the stuff we need!
    That includes functions...

---
+++ Functions/Functions.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <random>
+
+//Creates freelines
+void freeline(int num);
+//Input function
+std::string input(std::string info);
+//Print functions
+void print(std::string info);
+void println(std::string info);
+//Make strings uppercase and lowercase
+std::string upper(std::string upperWord);
+std::string lower(std::string lowerWord);
+//Create random number
+int random(int start, int end);
---
+++ Functions/main.cpp
@@ -0,0 +1,4 @@
+#include "functions.h"
+int main() {
+	system("PAUSE");
+}
